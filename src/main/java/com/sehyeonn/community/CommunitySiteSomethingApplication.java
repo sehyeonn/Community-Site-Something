e82402c0a2d45f6b73c5e0390bdfd70be5e16855
sehyeonn <a3920679@naver.com>
2022-02-26 21:05:31 +0900

    init

---
+++ src/main/java/com/sehyeonn/community/CommunitySiteSomethingApplication.java
@@ -0,0 +1,13 @@
+package com.sehyeonn.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitySiteSomethingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunitySiteSomethingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sehyeonn/community/CommunitySiteSomethingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sehyeonn.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitySiteSomethingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

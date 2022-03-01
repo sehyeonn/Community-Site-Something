2b797322dc8d08a2b9ff99c47dd99084d4ed04f5
sehyeonn <a3920679@naver.com>
2022-03-01 17:45:56 +0900

    PostMapper 메소드들에서 posteddatetime을 String으로 변환하도록 쿼리 수정, findById() 메소드 추가

--- src/main/java/com/sehyeonn/community/mapper/PostMapper.java
+++ src/main/java/com/sehyeonn/community/mapper/PostMapper.java
@@ -11,18 +11,24 @@ import com.sehyeonn.community.dto.Post;
 public interface PostMapper {
 	
 	// 게시글 목록 offset부터 rowCount개 조회 (최신순 정렬)
-	@Select("SELECT post.id, title, categoryId, postedDatetime, hits, username " +
+	@Select("SELECT post.id, title, categoryId, DATE_FORMAT(postedDatetime, \'%Y-%m-%d\') AS postedDatetime, hits, username " +
 			"FROM post JOIN user ON post.userId=user.id " +
-			"ORDER BY post.id DESC" +
+			"ORDER BY post.id DESC " +
 			"LIMIT #{offset}, #{rowCount}")
 	List<Post> findList(int offset, int rowCount);
 	
 	// 해당 카테고리의 게시글 목록 offset부터 rowCount개 조회 (최신순 정렬)
-	@Select("SELECT post.id, title, categoryId, postedDatetime, hits, username " +
+	@Select("SELECT post.id, title, categoryId, DATE_FORMAT(postedDatetime, \'%Y-%m-%d\') AS postedDatetime, hits, username " +
 			"FROM post JOIN user ON post.userId=user.id " +
 			"WHERE categoryId=#{categoryId} " +
 			"ORDER BY post.id DESC"+
 			"LIMIT #{offset}, #{rowCount}")
 	List<Post> findListByCategory(int categoryId, int offset, int rowCount);
+	
+	// 게시글 아이디로 게시글 하나 조회하는 메소드
+	@Select("SELECT post.id, title, username, content, username, categoryId, DATE_FORMAT(postedDatetime, \\'%Y-%m-%d\\') AS postedDatetime, DATE_FORMAT(updatedDatetime, \\\\'%Y-%m-%d\\\\') AS updatedDatetime" +
+			"FROM post JOIN user ON post.userId=user.id " +
+			"WHERE post.id=#{postId}")
+	Post findById(int postId);
 
 }

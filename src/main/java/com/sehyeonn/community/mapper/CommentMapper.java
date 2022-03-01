1ceb4dd5680d7eda004952d947619f7392638b22
sehyeonn <a3920679@naver.com>
2022-03-01 17:50:23 +0900

    CommentMapper 생성 및 countCommentByPostId, findAllCommentByPostId 메소드 작성

---
+++ src/main/java/com/sehyeonn/community/mapper/CommentMapper.java
@@ -0,0 +1,23 @@
+package com.sehyeonn.community.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.sehyeonn.community.dto.Comment;
+
+@Mapper
+public interface CommentMapper {
+	
+	// 해당 게시글의 댓글 개수를 받환하는 메소드
+	@Select("SELECT count(comment.id) FROM comment WHERE postId=#{postId}")
+	int countCommentByPostID(int postId);
+	
+	// 햐당 게시글의 댓글들을 반환하는 메소드
+	@Select("SELECT comment.id, username, postId, content, DATE_FORMAT(postedDatetime, \\'%Y-%m-%d\\') AS postedDatetime " +
+			"FROM comment JOIN user ON comment.userId=user.id" +
+			"WHERE comment.postId=#{postId}")
+	List<Comment> findAllCommentsByPostId(int postId);
+
+}

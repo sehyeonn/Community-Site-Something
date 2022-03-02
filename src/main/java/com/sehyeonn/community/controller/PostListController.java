73df63da8283c1bcc2a0e02735da058859bb273c
sehyeonn <a3920679@naver.com>
2022-03-02 22:46:11 +0900

    카테고리 별로 게시글 목록을 조회할 수 있도록 PostMapper findList 메소드 수정 및 PostController 수정

--- src/main/java/com/sehyeonn/community/controller/PostListController.java
+++ src/main/java/com/sehyeonn/community/controller/PostListController.java
@@ -1,10 +1,13 @@
 package com.sehyeonn.community.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.sehyeonn.community.dto.Post;
 import com.sehyeonn.community.mapper.PostMapper;
 
 @Controller
@@ -12,11 +15,24 @@ public class PostListController {
 	
 	@Autowired PostMapper postMapper;
 	
-	@GetMapping("main/postList")
-	public String postList(Model model, String searchText) {
+	@GetMapping("main/postList")	// 선택한 카테고리, 조회 중인 페이지 번호, 검색할 내용을 request parameter 로 받음
+	public String postList(Model model, Integer categoryId, Integer pageNumber, String searchText) {
+		if(pageNumber == null) pageNumber = 1;	// 처음엔 1페이지부터 조회
 		if(searchText == null) searchText = "";
-		model.addAttribute("posts", postMapper.findListBySearchText("%"+searchText+"%", 0, 30));
+		
+		List<Post> postList;
+		
+		if(categoryId == null) {	// 처음엔 전체 게시물 목록부터 보여줌
+			postList = postMapper.findList("%"+searchText+"%", (pageNumber-1)*30, 30);
+			model.addAttribute("posts", postList);
+		} else {	// 이후 카테고리 선택 시 해당 카테고리의 게시글 목록을 보여줌
+			postList = postMapper.findListByCategory(categoryId, "%"+searchText+"%", (pageNumber-1)*30, 30);
+			model.addAttribute("posts", postList);
+		}
+		
 		model.addAttribute("searchText", searchText);
+		model.addAttribute("pageNumber", pageNumber);
+		
 		return "main/postList";
 	}
 
--- src/main/java/com/sehyeonn/community/mapper/PostMapper.java
+++ src/main/java/com/sehyeonn/community/mapper/PostMapper.java
@@ -9,30 +9,22 @@ import com.sehyeonn.community.dto.Post;
 
 @Mapper
 public interface PostMapper {
-	
-	// 게시글 목록 offset부터 rowCount개 조회 (최신순 정렬)
-	@Select("SELECT post.id, title, categoryId, DATE_FORMAT(postedDatetime, \'%Y-%m-%d\') AS postedDatetime, hits, username " +
-			"FROM post JOIN user ON post.userId=user.id " +
-			"ORDER BY post.id DESC " +
-			"LIMIT #{offset}, #{rowCount}")
-	List<Post> findList(int offset, int rowCount);
 
-	// 게시글 검색을 위한 메소드, 제목이나 내용에 searchText가 포함되어 있는 게시글 목록을 반환
+	// 게시글 목록 offset부터 rowCount개 조회, 검색할 내용이 있다면 제목이나 내용에 searchText가 포함되어 있는 게시글 목록을 반환 (최신순 정렬)
 	@Select("SELECT post.id, title, categoryId, DATE_FORMAT(postedDatetime, \'%Y-%m-%d\') AS postedDatetime, hits, username " +
 			"FROM post JOIN user ON post.userId=user.id " +
 			"WHERE title LIKE #{searchText} OR content LIKE #{searchText} " +
 			"ORDER BY post.id DESC " +
 			"LIMIT #{offset}, #{rowCount}")
-	List<Post> findListBySearchText(String searchText, int offset, int rowCount);
+	List<Post> findList(String searchText, int offset, int rowCount);
 	
-	// 해당 카테고리의 게시글 목록 offset부터 rowCount개 조회 (최신순 정렬)
+	// 해당 카테고리의 게시글 목록 offset부터 rowCount개 조회, 검색할 내용이 있다면 제목이나 내용에 searchText가 포함되어 있는 게시글 목록을 반환 (최신순 정렬)
 	@Select("SELECT post.id, title, categoryId, DATE_FORMAT(postedDatetime, \'%Y-%m-%d\') AS postedDatetime, hits, username " +
 			"FROM post JOIN user ON post.userId=user.id " +
-			"WHERE categoryId=#{categoryId} " +
+			"WHERE categoryId=#{categoryId} AND (title LIKE #{searchText} OR content LIKE #{searchText}) " +
 			"ORDER BY post.id DESC "+
 			"LIMIT #{offset}, #{rowCount}")
-	List<Post> findListByCategory(int categoryId, int offset, int rowCount);
-	
+	List<Post> findListByCategory(int categoryId, String searchText, int offset, int rowCount);
 	
 	// 게시글 아이디로 게시글 하나 조회하는 메소드
 	@Select("SELECT post.id, title, username, content, username, categoryId, DATE_FORMAT(postedDatetime, \'%Y-%m-%d %H:%i\') AS postedDatetime, DATE_FORMAT(updatedDatetime, \'%Y-%m-%d %H:%i\') AS updatedDatetime " +

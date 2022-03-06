89d2c6d8f8ca3ba64e274c4a114f36f65e9464a0
sehyeonn <a3920679@naver.com>
2022-03-06 20:55:35 +0900

    PostMapper 게시글 등록, 게시글 수정 메소드 추가

--- src/main/java/com/sehyeonn/community/mapper/PostMapper.java
+++ src/main/java/com/sehyeonn/community/mapper/PostMapper.java
@@ -2,8 +2,11 @@ package com.sehyeonn.community.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.sehyeonn.community.dto.Post;
 
@@ -31,5 +34,16 @@ public interface PostMapper {
 			"FROM post JOIN user ON post.userId=user.id " +
 			"WHERE post.id=#{postId}")
 	Post findById(int postId);
+	
+	// 게시글 등록
+	@Insert("INSERT post (userId, categoryId, title, content, postedDatetime) " +
+			"VALUE (#{userId}, #{categoryId}, #{title}, #{content}, #{postedDatetime})")
+	@Options(useGeneratedKeys=true, keyProperty="id")
+	void insert(Post post);
+	
+	// 게시글 수정
+	@Update("UPDATE post SET categoryId=#{categoryId}, title=#{title}, content=#{content}, updatedDatetime=#{updatedDatetime} " +
+			"WHERE id=#{id}")
+	void update(Post post);
 
 }

b545bed77709ee543b21f2270c31b97a448ee50b
sehyeonn <a3920679@naver.com>
2022-03-24 21:30:17 +0900

    Crypto 클래스에 salt 값 생성 함수, 해싱 함수 작성

--- src/main/java/com/sehyeonn/community/utility/Crypto.java
+++ src/main/java/com/sehyeonn/community/utility/Crypto.java
@@ -1,6 +1,43 @@
 package com.sehyeonn.community.utility;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 // 회원가입, 로그인 시 salt 값 생성, 비밀번호 해싱, 비교를 위한 클래스
 public class Crypto {
+	
+	private static final int SALT_SIZE = 10;	// salt값의 크기
+	
+	// salt값 생성
+	public static String createSalt() {
+		SecureRandom random = new SecureRandom();
+		byte[] salt = new byte[SALT_SIZE];
+		random.nextBytes(salt);
+		
+		return new String(salt);
+	}
+	
+	// 비밀번호를 salt값과 함께 해싱하여 리턴
+	public static String hashing(String password, String salt) {
+		try {
+			return doHashing(password, salt);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return "error";
+		}
+	}
+	private static String doHashing(String password, String salt) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA-512");
+		
+		// key stretching
+		for(int i=0; i<10000; i++) {
+			String temp = password + salt;		// 비밀번호와 salt를 합친 문자열 생성
+			md.update(temp.getBytes());			// 문자열 해싱
+			password = new String(md.digest());	// 다이제스트를 얻어 password 갱신
+		}
+		
+		return password;
+	}
 	
 }

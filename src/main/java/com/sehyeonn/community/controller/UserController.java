0a790873e017ad180f488e13879d04dfec637f06
sehyeonn <a3920679@naver.com>
2022-03-25 21:47:51 +0900

    UserController에서 로그인 및 회원가입 시 비밀번호를 salt값과 함께 조회 및 데이터베이스에 저장하도록 수정

--- src/main/java/com/sehyeonn/community/controller/UserController.java
+++ src/main/java/com/sehyeonn/community/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sehyeonn.community.dto.User;
 import com.sehyeonn.community.mapper.UserMapper;
+import com.sehyeonn.community.utility.Crypto;
 
 @Controller
 public class UserController {
@@ -29,15 +30,21 @@ public class UserController {
 		String errorMsg = "";
 		User user = userMapper.findById(userId);
 		
-		if(userId == null || userId.length() == 0) {
+		if(user != null) {	// 입력받은 아이디의 유저가 존재한다면 입력 받은 비밀번호와 해당 유저의 salt와 함께 해싱
+			try {
+				password = Crypto.hashing(password, user.getSalt());
+			} catch (Exception e) { e.printStackTrace();	}
+		}
+		
+		if(userId == null || userId.length() == 0) {	// 아이디가 입력되지 않았을 때
 			errorMsg = "Enter your ID";
-		} else if(password == null || password.length() == 0) {
+		} else if(password == null || password.length() == 0) {		// 비밀번호가 입력되지 않았을 때
 			errorMsg = "Enter your PASSWORD";
-		} else if(user == null) {
+		} else if(user == null) {	// 입력 받은 아이디에 해당하는 유저가 존재하지 않을 때
 			errorMsg = "The ID doesn\'t exist";
-		} else if(user.getPassword().equals(password) == false) {
+		} else if(user.getPassword().equals(password) == false) {	// 비밀번호가 일치하지 않을 때
 			errorMsg = "The password doesn\'t match";
-		} else {
+		} else {	// 로그인 성공
 			session.setAttribute("user", user);	// 세션에 로그인한 유저 저장
 			return "redirect:/main/postList";	// 임시로 설정해 놓은 뷰, 메인페이지가 생기면 바꿀 것
 		}
@@ -66,8 +73,16 @@ public class UserController {
 			User newUser = new User();
 			newUser.setId(userId);
 			newUser.setPassword(password);
+			
+			String salt = Crypto.createSalt();	// 새로운 유저의 고유 salt값 생성
+			newUser.setSalt(salt);
+			try {
+				newUser.setPassword(Crypto.hashing(password, salt));	// 비밀번호를 salt와 함께 해싱하여 데이터베이스에 저장
+			} catch (Exception e) { e.printStackTrace(); }
+			
 			newUser.setUsername(username);
 			newUser.setSignupDate(new Date());
+			
 			userMapper.insert(newUser);
 			
 			return "redirect:signupSuccessful";	// 회원가입 성공 페이지로 이동
